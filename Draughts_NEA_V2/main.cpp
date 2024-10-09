2cca07f0681d4908ec83abba36faf598a56183cb
Daniel <daniel.matveev07@gmail.com>
2024-10-09 11:06:59 +0100

    Initial Commit

---
+++ Draughts_NEA_V2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Draughts_NEA_V2
+//
+//  Created by Daniel Matveev on 09/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
