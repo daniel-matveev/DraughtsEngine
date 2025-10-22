b4839d43d940aa6f75c847fab8e29358afd93ed6
piekarate <92880613+piekarate@users.noreply.github.com>
2025-10-22 21:56:28 +0100

    Delete DerivedData directory

--- DerivedData/Draughts_NEA_V2/Build/Intermediates.noindex/Draughts_NEA_V2.build/Debug/Draughts_NEA_V2.build/Objects-normal/x86_64/AlphaBetaPruning.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/Draughts_NEA_V2/AlphaBetaPruning.cpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/AlphaBetaPruning.hpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/Game.hpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/Board.hpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/Piece.hpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/Utilities.hpp
--- DerivedData/Draughts_NEA_V2/Build/Intermediates.noindex/Draughts_NEA_V2.build/Debug/Draughts_NEA_V2.build/Objects-normal/x86_64/Utilities.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/Draughts_NEA_V2/Utilities.cpp \
-  /Users/danielmatveev/Desktop/School/Computer\ Science/Draughts_NEA_V2/HeaderFiles/Utilities.hpp
